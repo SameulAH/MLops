d00e109f3fbeecf417e8cdae90e3cbb0f8b78c42
Sam <62577473+SameulAH@users.noreply.github.com>
2025-06-07 05:37:58 +0200

    minio_ingest

--- orchestration/dags/ingest_dag.py
+++ orchestration/dags/ingest_dag.py
@@ -1,24 +1,41 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+# dags/ingest_to_minio_dag.py
+import sys
+import os
+from airflow.decorators import dag, task
 from datetime import datetime
 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from ingest import upload_to_minio
+
 default_args = {
-    "owner": "airflow",
+    "owner": "airflow1",
     "start_date": datetime(2024, 1, 1),
-    "retries": 1,
+    "retries": 2
 }
 
-with DAG(
-    dag_id="data_ingestion_pipeline",
+@dag(
+    dag_id="yellow_trip_ingestminio1",
     default_args=default_args,
-    description="Ingest green taxi data into PostgreSQL",
-    schedule_interval=None,  # run manually unless scheduled
+    description="Download Yellow Taxi data to store in MinIO",
+    schedule_interval=None,
     catchup=False,
-) as dag:
+    tags=["yellow", "minioingest", "ingestion"]
+)
+def yellow_ingest_dag():
 
-    ingest_task = BashOperator(
-        task_id="run_data_ingestion",
-        bash_command="python /opt/airflow/ingest.py"  # Update path if needed
-    )
+    @task()
+    def ingest_dataset(url: str, filename: str):
+        upload_to_minio(url, filename, folder="raw")
 
-    ingest_task
+    urls = {
+        "yellow_tripdata_2023-01.parquet": "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet",
+        "yellow_tripdata_2023-02.parquet": "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-02.parquet",
+        "yellow_tripdata_2023-03.parquet": "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet",
+    }
+
+    for filename, url in urls.items():
+        ingest_dataset.override(task_id=f"ingest_{filename.replace('.parquet', '')}")(url, filename)
+
+dag_instance = yellow_ingest_dag()
